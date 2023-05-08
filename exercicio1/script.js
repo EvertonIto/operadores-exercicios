1fc74d59e4b800fe46b4c72d76e9b49a1e224135
Everton Ito <ebatista.ito@gmail.com>
2023-05-07 21:28:52 -0300

    Exercicio ressolvido

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+let numeroPar = Number(prompt("Digite um número par"))
+console.log(numeroPar)
+
+let resto = numeroPar % 2
+console.log(resto)
+// numeros pares o resto sempre é 0 
+// numeros impares o resto sempre é 1
